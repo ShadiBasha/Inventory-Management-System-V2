b9210d5dd95a3e2392a96956b96c29a589616e21
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-06 16:12:53 +0200

    Add a product: Creation of product class
    This is the class that will represent the product. It has three fields
    for the instance (product number, price, and quantity), one static field
    for the class (number of products) to keep track of the product count,
    and four properties (Name, Price, Quantity, IsNull). Additionally, it
    includes a default constructor.

---
+++ InventoryManagementSyetemV2/Product.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlTypes;
+
+namespace InventoryManagementSyetemV2;
+
+public class Product : INullable
+{
+    private static int _numberOfProducts;
+    private int _productNumber;
+    private int _price;
+    private int _quantity;
+    
+    public string Name { get; set; }
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (value < 0)
+                _quantity = 0;
+            else
+                _quantity = value;
+        }
+    }
+    
+    public int Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+                _price = 0;
+            else
+                _price = value;
+        }
+    }
+
+    public bool IsNull { get; set; }
+    
+    
+    public Product()
+    {
+        IsNull = true;
+        _productNumber = -1;
+        Name = "";
+        Price = 0;
+        Quantity = 0;
+    } 
+    
+}
